9ce86dab70a9ff26fca6a8d6315400cbb7f769c2
Kavya-rajkumar99 <kavyarajkumar99@gmail.com>
2022-06-01 12:24:16 +0530

    Created using react

--- src/App.js
+++ src/App.js
@@ -2,24 +2,43 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = "Kavya"
+  const details=[
+    {
+      name : "Kavya",
+      pic:"https://1.bp.blogspot.com/-TUa3v957y_4/YRDJpRzW6NI/AAAAAAAAM9A/W1tQHdwoYhccVEUm4L2qn_As_kf8ggI8ACLcBGAsYHQ/s540/alone-Whatsapp-Dp-Images-alone-girl-dp-Alone-Girl-Dp-For-Facebook-alone-Whatsapp-Dp-Images-sad-alone-girl-dp-alone-girl-dp%2B%25282%2529.jpg"
+    },
+    {
+      name : "Krithika",
+      pic:"https://www.attitudestatus.org/wp-content/uploads/2021/05/boy-girl-dps-1.jpg"
+    },
+    {
+      name : "Varnika",
+      pic:"https://wallpapercave.com/wp/wp5135903.jpg"
+    }
+
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      {
+        details.map(person => <Message name={person.name} pic={person.pic} />)
+      }
+    {/* <Message name="Kavya" pic="https://1.bp.blogspot.com/-TUa3v957y_4/YRDJpRzW6NI/AAAAAAAAM9A/W1tQHdwoYhccVEUm4L2qn_As_kf8ggI8ACLcBGAsYHQ/s540/alone-Whatsapp-Dp-Images-alone-girl-dp-Alone-Girl-Dp-For-Facebook-alone-Whatsapp-Dp-Images-sad-alone-girl-dp-alone-girl-dp%2B%25282%2529.jpg" />
+    <Message name="Krithika" pic="https://www.attitudestatus.org/wp-content/uploads/2021/05/boy-girl-dps-1.jpg" />
+    <Message name="Varnika" pic="https://wallpapercave.com/wp/wp5135903.jpg" /> */}
+  </div>
   );
 }
 
+function Message({name,pic}){
+  return(
+  <div>
+   <img className="profile-pic" src={pic} alt={name}/>
+   <h1>Hello {name}</h1>
+  </div>
+  
+  );
+}
 export default App;
+
+
